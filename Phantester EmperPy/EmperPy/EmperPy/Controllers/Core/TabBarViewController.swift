38e7f37031441ea2e1ee319230b9003d7b6a9fdb
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-05-17 14:30:50 +0900

    Add files via upload

--- Phantester EmperPy/EmperPy/EmperPy/Controllers/Core/TabBarViewController.swift
+++ Phantester EmperPy/EmperPy/EmperPy/Controllers/Core/TabBarViewController.swift
@@ -12,7 +12,28 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let core1 = HomeViewController()
+        let core2 = SearchViewController()
+        let core3 = LibraryViewController()
+        
+        core1.title = "Browse"
+        core2.title = "search"
+        core3.title = "Library"
+        
+        core1.navigationItem.largeTitleDisplayMode = .always
+        core2.navigationItem.largeTitleDisplayMode = .always
+        core3.navigationItem.largeTitleDisplayMode = .always
+        
+        let nav1 = UINavigationController(rootViewController: core1)
+        let nav2 = UINavigationController(rootViewController: core2)
+        let nav3 = UINavigationController(rootViewController: core3)
+        
+        nav1.navigationBar.prefersLargeTitles = true
+        nav2.navigationBar.prefersLargeTitles = true
+        nav3.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers(([nav1, nav2, nav3]), animated: false)
+        
     }
 
 }
